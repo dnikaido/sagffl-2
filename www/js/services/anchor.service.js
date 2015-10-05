8025db2d8a7f9d0632994588db7674b3553b176f
dnikaido <david.nikaido@gmail.com>
2015-10-05 18:38:56 -0500

    Fixed tab state

--- www/js/services/anchor.service.js
+++ www/js/services/anchor.service.js
@@ -10,15 +10,16 @@
     loadStates();
 
     function loadStates() {
-      service.states.push(new State('nav.home', 'Home'));
+      service.states.push(new State('nav.home', 'Home', true));
       service.states.push(new State('nav.announcements', 'Announcements'));
-      service.states.push(new State('nav.images', 'Images'));
-      service.states.push(new State('nav.programs', 'Programs'));
+      service.states.push(new State('nav.images', 'Images', true));
+      service.states.push(new State('nav.programs', 'Programs', true));
     }
 
-    function State(state, name) {
+    function State(state, name, tab) {
       this.state = state;
       this.name = name;
+      this.tab = tab;
     }
   }
 })();
--- www/templates/nav/nav.js
+++ www/templates/nav/nav.js
@@ -6,28 +6,18 @@
 
   function NavController($state, Anchor, $ionicTabsDelegate) {
     var vm = this;
-    var visibleTabs = ['nav.home', 'nav.programs', 'nav.images'];
 
     vm.$state = $state;
     vm.menuItems = Anchor.states;
-    vm.tabItems = [];
+    vm.tabItems = Anchor.states.filter(isTab);
     vm.selectTab = selectTab;
 
-    activate();
-
-    function activate() {
-      vm.tabItems = Anchor.states.filter(isInVisibleTabs);
-    }
-
-    function isInVisibleTabs(item) {
-      return 'state' in item && visibleTabs.indexOf(item.state) >= 0;
+    function isTab(item) {
+      return item.tab===true;
     }
 
     function selectTab(index) {
-      $ionicTabsDelegate.$getByHandle('main').select(index);
-      //Tabs wouldn't work if the same tab was selected while in a nested state
-      if(index===2) $state.go('nav.programs');
+      $state.go(vm.tabItems[index].state);
     }
-
   }
 })();
